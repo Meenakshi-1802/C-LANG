656c5dd6d960883c18e0a43a36e87503fddf1407
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-21 18:43:48 +0530

    Comments

---
+++ 02pro.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    //this is a single line comment in c
+    printf("Hello to c program\n");
+    /*
+    this is a
+    multiline comment 
+    in c
+    */
+   printf("Thank You!");
+    return 0;
+}
