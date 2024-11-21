b3d20e123ab74b5f2c813f147378e8a261295be7
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-21 21:55:50 +0530

    Variables

---
+++ 03pro.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int age = 21;
+    printf("Age is %d\n",age);
+    return 0;
+}
+
+#include<stdio.h>
+int main(){
+    float pi = 3.14;
+    printf("Pi is %f\n",pi);
+    return 0;
+}
+
+#include<stdio.h>
+int main(){
+    char hastag = '#';
+    printf("character is %c\n",hastag);
+    return 0;
+}
