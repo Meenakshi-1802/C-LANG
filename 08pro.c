46d1dd716091f5fc41eeb47ab700b15ec7291949
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-22 21:21:05 +0530

    Precedence

---
+++ 08pro.c
@@ -0,0 +1,28 @@
+Operator Precedence
+#include<stdio.h>
+int main() {
+    int a = 4*3/6*2;
+    printf("%d\n",a);
+    return 0;
+}
+
+#include<stdio.h>
+int main() {
+int a = 5*2-2*3;
+printf("%d\n",a);
+    return 0;
+}
+
+#include<stdio.h>
+int main() {
+int a = 5*(2/2)*3;
+printf("%d\n",a);
+    return 0;
+}
+
+#include<stdio.h>
+int main() {
+int a = 5+(2/2)*3;
+printf("%d\n",a);
+    return 0;
+}
