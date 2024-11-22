357852b5a33fe943c643c585bbb2058eaae4597a
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-22 08:33:53 +0530

    Conversion

---
+++ 07pro.c
@@ -0,0 +1,22 @@
+//Type Conversion
+//(int-int)
+#include<stdio.h>
+int main() {
+    printf("%d",2/3);
+    return 0;
+}
+
+//(int-float)
+#include<stdio.h>
+int main() {
+    printf("%f",4/3.0);
+    return 0;
+ }
+
+//(float-float)
+#include<stdio.h>
+int main() {
+    printf("%f",2.0*2.0);
+    return 0;
+}
+
