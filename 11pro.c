a208213dfc6b20db55e73d21651a04a032daf7be
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-23 10:51:12 +0530

    Ternary operator

---
+++ 11pro.c
@@ -0,0 +1,9 @@
+//Ternary operator
+ #include<stdio.h>
+   int main() {
+   int age;
+   printf("Enter your age:");
+   scanf("%d",&age);
+   age >= 18 ? printf("adult\n") :printf("not adult\n");
+    return 0;
+   }
