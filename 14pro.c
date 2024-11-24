34b0636c07c115247ae749ecf4e26bd7727fb711
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-24 10:41:01 +0530

    Increment operator

---
+++ 14pro.c
@@ -0,0 +1,19 @@
+//increment operator
+#include<stdio.h>
+int main() {
+    int i =1;
+    printf("%d\n",i++);//use,then increase
+    printf("%d\n",i);
+
+    return 0;
+}
+
+#include<stdio.h>
+int main() {
+    int i =1;
+    printf("%d\n",++i);//increase,then use
+    printf("%d\n",i);
+
+    return 0;
+}
+
