2475c3a6ab4dbcef3be93bb650bae59925d78259
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-24 10:56:50 +0530

    Decrement operator

---
+++ 15pro.c
@@ -0,0 +1,16 @@
+//Decrement operator
+#include<stdio.h>
+int main() {
+int i = 1;
+printf("%d\n",i--);//post decrement
+printf("%d\n",i);
+return 0;
+}
+
+#include<stdio.h>
+int main() {
+int i = 1;
+printf("%d\n",--i);//pre decrement
+printf("%d\n",i);
+return 0;
+}
