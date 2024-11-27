cb12fc91a4ff6d9e6c35b790a7c225aa6bccd9b0
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-11-27 08:34:09 +0530

    update

--- 21pro.c
+++ 21pro.c
@@ -1,39 +1,39 @@
 //Question-1
 //print hello world
-// #include<stdio.h>
-// int main() {
-// printf("Hello World!");
-//     return 0;
-// }
+#include<stdio.h>
+int main() {
+printf("Hello World!");
+    return 0;
+}
 
 //Question-2
 //print the value of X
-// #include<stdio.h>
-// int main() {
-//     int x = 10;
-// printf("%d\n",x);
-//     return 0;
-// }
+#include<stdio.h>
+int main() {
+    int x = 10;
+printf("%d\n",x);
+    return 0;
+}
 
 //Question-3
 //calculate & print the sum
-// #include<stdio.h>
-// int main() {
-// int x = 7;
-// int y = 5;
-// int sum = x + y;
-// printf("sum is %d\n",sum);
-//     return 0;
-// }
+#include<stdio.h>
+int main() {
+int x = 7;
+int y = 5;
+int sum = x + y;
+printf("sum is %d\n",sum);
+    return 0;
+}
 
 //Question-4
 //find the remainder of division
-// #include<stdio.h>
-// int main() {
-//     int remainder = 17%3;
-//     printf("remainder is %d\n",remainder);
-//     return 0;
-// }
+#include<stdio.h>
+int main() {
+    int remainder = 17%3;
+    printf("remainder is %d\n",remainder);
+    return 0;
+}
 
 //Question-5
 //convert celsius to fahrenheit
